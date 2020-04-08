4aa394457af5f45e1a89fd1bfd4759403c161228
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-08 19:15:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,7 @@ class Artist
     @songs << song
   end
 
-  def sef.find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.create(name)
   end
 end
