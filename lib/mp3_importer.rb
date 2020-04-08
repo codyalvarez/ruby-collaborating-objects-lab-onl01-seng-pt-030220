5d4b96651becd24cd31d4b0c8885e53196466e78
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-08 19:20:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,6 @@
 class MP3Importer
-  attr_reader :path, :files
+  attr_reader :path 
 
   def initialize(path)
-    @path = path
-    @files = Dir.entries(path).grep(/.*\.mp3/)
+    @path = path 
   end
-
-  def import
-    @files.each {|file| Song.new_by_filename(file)}
-  end
-end
