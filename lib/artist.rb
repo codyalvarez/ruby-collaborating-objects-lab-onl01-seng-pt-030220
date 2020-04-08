e4eb58e0d1495c1a074690c3073ab7ce77f3463b
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-08 20:34:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,10 +24,10 @@ class Artist
   def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
     if found_artist
-      found_artist
+      return found_artist
     else
       new_artist = self.new(artist_name)
-      new_artist
+    return new_artist
     end
   end
 
