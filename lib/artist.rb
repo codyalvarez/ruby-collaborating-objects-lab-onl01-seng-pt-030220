a20ce9810c07a593197dc48681c286d199708690
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-08 19:16:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,4 +18,8 @@ class Artist
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.create(name)
   end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
 end
